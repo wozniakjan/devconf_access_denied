7a237f4948e548a1fd400fb9db44a7680e028b53
Jan Wozniak <jwozniak@redhat.com>
2018-01-17 17:57:13 +0100

    add selinux labels and other minor tweaks

--- 1/init.bash
+++ 1/init.bash
@@ -1,6 +1,12 @@
 set -e
 
-touch /home/$USER/pv/1/file
-chown 1001 /home/$USER/pv/1/file
+#fs
+touch /home/$user/pv/1/file
+chown 1000 /home/$user/pv/1/file
 
-oc create -f <(cat /root/init/1/pod.yaml <(echo "        path: /home/$USER/pv/1"))
+#SELinux
+chcon -u system_u -r object_r -t svirt_sandbox_file_t /home/$user/pv/1
+chcon -u system_u -r object_r -t svirt_sandbox_file_t /home/$user/pv/1/file
+
+#create pod
+oc create -f <(cat /root/init/1/pod.yaml <(echo "        path: /home/$user/pv/1"))
--- init_user.bash
+++ init_user.bash
@@ -15,38 +15,38 @@ info() {
 TEMP=`getopt -o u:p:h --long user:,password:,help -n 'init' -- "$@"`
 eval set -- "$TEMP"
 
-USER=""
+user=""
 while true ; do
     case "$1" in
-        -u|--user) export USER="$2" ; shift 2 ;;
-        -p|--password) export PASSWORD="$2" ; shift 2 ;;
+        -u|--user) export user="$2" ; shift 2 ;;
+        -p|--password) export pass="$2" ; shift 2 ;;
         -h|--help) usage ; shift 1 ;;
         --) shift ; break ;;
         *) usage ; exit 1 ;;
     esac
 done
 
-if [[ -z $USER ]]; then
+if [[ -z $user ]]; then
 	usage
 	exit 1
 fi
-if [[ -z $PASSWORD ]]; then
-    PASSWORD=$USER	
+if [[ -z $pass ]]; then
+    pass=$user	
 fi
 
-info "adding user $USER"
-useradd -g users -d /home/$USER -s /bin/bash -p $(echo  $PASSWORD | openssl passwd -1 -stdin) $USER
+info "adding user $user"
+useradd -g users -d /home/$user -s /bin/bash -p $(echo  $pass | openssl passwd -1 -stdin) $user
 
 info "initializing oc user and project"
-htpasswd -b /root/pwd $USER $PASSWORD
-oc login -u $USER -p $PASSWORD 
-oc new-project ${USER}-project 
+htpasswd -b /root/pwd $user $pass
+oc login -u $user -p $pass 
+oc new-project ${user}-project 
 oc login -u system:admin 
-oc adm policy add-scc-to-user hostaccess $USER
-oc login -u $USER -p $PASSWORD 
+oc adm policy add-scc-to-user hostaccess $user
+oc login -u $user -p $pass 
 
 info "initializing tasks"
-mkdir -p /home/$USER/pv/{1..15} 
+mkdir -p /home/$user/pv/{1..15} 
 for f in {1..15}; do
     /root/init/$f/init.bash 
 done
