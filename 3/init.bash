0f185464ed4bfa6339eed0a5aecd2db75f5d9f32
Jan Wozniak <jwozniak@redhat.com>
2018-01-22 14:53:15 +0100

    added third task

---
+++ 3/init.bash
@@ -0,0 +1,22 @@
+set -e
+
+# SElinux securityContext level and categories missing
+#
+# oc edit deployment
+#   securityContext:
+#     seLinuxOptions:
+#       level: s0,c0,c5
+
+# fs
+touch /home/$user/pv/3/file
+chown $user /home/$user/pv/3/file
+chmod 664 /home/$user/pv/3/file
+
+# SELinux
+chcon -u system_u -r object_r -t svirt_sandbox_file_t /home/$user/pv/3
+chcon -u system_u -r object_r -t svirt_sandbox_file_t /home/$user/pv/3/file
+chcat -- +c0 /home/$user/pv/3
+chcat -- +c5 /home/$user/pv/3
+
+# create dc 
+oc create -f <(cat /root/init/3/dc.yaml <(echo "          path: /home/$user/pv/3"))
